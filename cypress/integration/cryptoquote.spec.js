06afeae895463845a5ec5909a9ddd93f19659be0
Calaway <calaway@users.noreply.github.com>
2020-12-19 23:34:02 -0700

    Delete character on Backspace or Delete

--- cypress/integration/cryptoquote.spec.js
+++ cypress/integration/cryptoquote.spec.js
@@ -10,16 +10,26 @@ describe('Cryptoquote', () => {
       cy.get('#plaintext').should('have.text', '____ ______ ____’_.');
     });
 
-    context('when assigning letters', () => {
+    context('when assigning and deleting letters', () => {
       it('the letters are filled in correctly', () => {
         cy.visit('http://localhost:3000');
 
         cy.get('#ciphertext-input').type('Ejwb zdbpwn psdh’r.');
         cy.get('[data-ciphertext-char="D"]').first().click().type('a');
+        cy.get('[data-ciphertext-char="B"]').first().click().type('b');
 
         cy.get('#ciphertext-input').should('have.value', 'EJWB ZDBPWN PSDH’R.');
         cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
-        cy.get('#plaintext').should('have.text', '____ _A____ __A_’_.');
+        cy.get('#plaintext').should('have.text', '___B _AB___ __A_’_.');
+
+        cy.get('[data-ciphertext-char="D"]')
+          .first()
+          .click()
+          .type('{Backspace}');
+
+        cy.get('#ciphertext-input').should('have.value', 'EJWB ZDBPWN PSDH’R.');
+        cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
+        cy.get('#plaintext').should('have.text', '___B __B___ ____’_.');
       });
     });
   });
