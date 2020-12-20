fe96d005c01b7a512dad4fb2a98effae5395efdb
Calaway <calaway@users.noreply.github.com>
2020-12-19 23:10:09 -0700

    Create dummy input field to capture input on mobile

--- cypress/integration/cryptoquote.spec.js
+++ cypress/integration/cryptoquote.spec.js
@@ -9,5 +9,18 @@ describe('Cryptoquote', () => {
       cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
       cy.get('#plaintext').should('have.text', '____ ______ ____’_.');
     });
+
+    context('when assigning letters', () => {
+      it('the letters are filled in correctly', () => {
+        cy.visit('http://localhost:3000');
+
+        cy.get('#ciphertext-input').type('Ejwb zdbpwn psdh’r.');
+        cy.get('[data-ciphertext-char="D"]').first().click().type('a');
+
+        cy.get('#ciphertext-input').should('have.value', 'EJWB ZDBPWN PSDH’R.');
+        cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
+        cy.get('#plaintext').should('have.text', '____ _A____ __A_’_.');
+      });
+    });
   });
 });
