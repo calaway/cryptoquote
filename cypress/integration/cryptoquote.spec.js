0d46c4394153b560bb8afa01c04baec7c58ac44b
Calaway <calaway@users.noreply.github.com>
2020-12-26 23:58:06 -0700

    Test delete via del key

--- cypress/integration/cryptoquote.spec.js
+++ cypress/integration/cryptoquote.spec.js
@@ -17,15 +17,17 @@ describe('Cryptoquote', () => {
         cy.get('#ciphertext-input').type('Ejwb zdbpwn psdh’r.');
         cy.get('[data-ciphertext-char="D"]').first().click().type('a');
         cy.get('[data-ciphertext-char="B"]').first().click().type('b');
+        cy.get('[data-ciphertext-char="E"]').first().click().type('C');
 
         cy.get('#ciphertext-input').should('have.value', 'EJWB ZDBPWN PSDH’R.');
         cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
-        cy.get('#plaintext').should('have.text', '___B _AB___ __A_’_.');
+        cy.get('#plaintext').should('have.text', 'C__B _AB___ __A_’_.');
 
         cy.get('[data-ciphertext-char="D"]')
           .first()
           .click()
-          .type('{Backspace}');
+          .type('{backspace}');
+        cy.get('[data-ciphertext-char="E"]').first().click().type('{del}');
 
         cy.get('#ciphertext-input').should('have.value', 'EJWB ZDBPWN PSDH’R.');
         cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
