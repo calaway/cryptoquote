02fff03b7daae26372bcc95066da406f206b9d50
Calaway <calaway@users.noreply.github.com>
2020-12-28 18:51:40 -0700

    Combine two tests into one

--- cypress/integration/cryptoquote.spec.js
+++ cypress/integration/cryptoquote.spec.js
@@ -1,5 +1,5 @@
 describe('Cryptoquote', () => {
-  context('when entering new ciphertext', () => {
+  context('when entering ciphertext, assigning, and deleting letters', () => {
     it('the correct ciphertext and plaintext is displayed', () => {
       cy.visit('http://localhost:3000');
 
@@ -8,31 +8,21 @@ describe('Cryptoquote', () => {
       cy.get('#ciphertext-input').should('have.value', 'Ejwb zdbpwn psdh’r.');
       cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
       cy.get('#plaintext').should('have.text', '____ ______ ____’_.');
-    });
 
-    context('when assigning and deleting letters', () => {
-      it('the letters are filled in correctly', () => {
-        cy.visit('http://localhost:3000');
+      cy.get('[data-ciphertext-char="D"]').first().click().type('a');
+      cy.get('[data-ciphertext-char="B"]').first().click().type('b');
+      cy.get('[data-ciphertext-char="E"]').first().click().type('C');
 
-        cy.get('#ciphertext-input').type('Ejwb zdbpwn psdh’r.');
-        cy.get('[data-ciphertext-char="D"]').first().click().type('a');
-        cy.get('[data-ciphertext-char="B"]').first().click().type('b');
-        cy.get('[data-ciphertext-char="E"]').first().click().type('C');
+      cy.get('#ciphertext-input').should('have.value', 'Ejwb zdbpwn psdh’r.');
+      cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
+      cy.get('#plaintext').should('have.text', 'C__B _AB___ __A_’_.');
 
-        cy.get('#ciphertext-input').should('have.value', 'Ejwb zdbpwn psdh’r.');
-        cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
-        cy.get('#plaintext').should('have.text', 'C__B _AB___ __A_’_.');
+      cy.get('[data-ciphertext-char="D"]').first().click().type('{backspace}');
+      cy.get('[data-ciphertext-char="E"]').first().click().type('{del}');
 
-        cy.get('[data-ciphertext-char="D"]')
-          .first()
-          .click()
-          .type('{backspace}');
-        cy.get('[data-ciphertext-char="E"]').first().click().type('{del}');
-
-        cy.get('#ciphertext-input').should('have.value', 'Ejwb zdbpwn psdh’r.');
-        cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
-        cy.get('#plaintext').should('have.text', '___B __B___ ____’_.');
-      });
+      cy.get('#ciphertext-input').should('have.value', 'Ejwb zdbpwn psdh’r.');
+      cy.get('#ciphertext').should('have.text', 'EJWB ZDBPWN PSDH’R.');
+      cy.get('#plaintext').should('have.text', '___B __B___ ____’_.');
     });
   });
 });
