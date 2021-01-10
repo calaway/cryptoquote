e7335014c890e64fa7760110e42744415c32f55c
Calaway <calaway@users.noreply.github.com>
2021-01-10 14:00:43 -0700

    Fix PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const OFFLINE_VERSION = 2;
+const CACHE_NAME = 'offline';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/src/words.txt',
+        'src/images/background.jpg',
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async () => {
+      try {
+        // Always try the network first.
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        console.log(
+          'Fetch failed; attempting to return cached asset.',
+          event.request.url
+        );
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request);
+        return cachedResponse;
+      }
+    })()
+  );
+});
